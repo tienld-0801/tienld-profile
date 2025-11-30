82b8e69a52182264c32fdbd5436673940b4d3293
tienld-tgl <tien.d.le@tgl-sol.net>
2025-11-30 11:19:36 +0700

    fix hidrad app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,40 @@
-import { Metadata } from 'next';
-import Head from 'next/head';
-import Script from 'next/script';
-import { Analytics } from '@vercel/analytics/next';
-import { Geist, Geist_Mono } from 'next/font/google';
-import { SpeedInsights } from '@vercel/speed-insights/next';
-import { Providers } from '@/shared/providers/providers';
-import { MotionLayout } from '@/shared/providers/layout-provider';
+import { Metadata, Viewport } from "next";
+import Script from "next/script";
+import { Analytics } from "@vercel/analytics/next";
+import { Geist, Geist_Mono } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Providers } from "@/shared/providers/providers";
+import { MotionLayout } from "@/shared/providers/layout-provider";
 
-import './globals.css';
+import "./globals.css";
 
-export const dynamic = 'force-static';
+export const dynamic = "force-static";
 
 const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+};
+
 export const metadata: Metadata = {
-  title: 'Welcome to my profile !',
-  applicationName: 'Profile',
-  description: 'TienLeDuy',
+  title: "Welcome to my profile !",
+  applicationName: "Profile",
+  description: "TienLeDuy",
   authors: {
-    url: '',
-    name: 'TienLeDuy',
+    url: "",
+    name: "TienLeDuy",
   },
   icons: {
-    icon: '/globe.svg',
+    icon: "/globe.svg",
   },
 };
 
@@ -41,23 +45,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <Head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-2RTT04K4WS"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`  window.dataLayer = window.dataLayer || [];
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
+      >
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-2RTT04K4WS"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`  window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-2RTT04K4WS');`}
-      </Script>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        </Script>
         <Providers>
           <MotionLayout>
             {children}
