c4b46fbe96cb05debde36914d9c62991e3f2ca22
tienld-tgl <tien.d.le@tgl-sol.net>
2025-11-30 11:16:50 +0700

    remove build file

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,23 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
   compiler:
-    process.env.NODE_ENV === 'production'
+    process.env.NODE_ENV === "production"
       ? {
-          removeConsole: { exclude: ['error'] },
+          removeConsole: { exclude: ["error"] },
           styledComponents: true,
         }
       : {},
   async headers() {
     return [
       {
-        source: '/:all*(svg|jpg|png)',
+        source: "/:all*(svg|jpg|png)",
         locale: false,
         headers: [
           {
-            key: 'Cache-Control',
-            value: 'public, max-age=9999999999, must-revalidate',
+            key: "Cache-Control",
+            value: "public, max-age=9999999999, must-revalidate",
           },
         ],
       },
@@ -26,28 +26,18 @@ const nextConfig: NextConfig = {
   images: {
     minimumCacheTTL: 31536000,
   },
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
       },
-      resolveAlias: {
-        underscore: 'lodash',
-        mocha: { browser: 'mocha/browser-entry.js' },
-      },
-      resolveExtensions: [
-        '.mdx',
-        '.tsx',
-        '.ts',
-        '.jsx',
-        '.js',
-        '.mjs',
-        '.json',
-      ],
     },
+    resolveAlias: {
+      underscore: "lodash",
+      mocha: { browser: "mocha/browser-entry.js" },
+    },
+    resolveExtensions: [".mdx", ".tsx", ".ts", ".jsx", ".js", ".mjs", ".json"],
   },
 };
 
